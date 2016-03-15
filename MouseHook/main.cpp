c53eaadbc4be1fc908c02f91c3eb07eda1de8f84
quartorz <tsuhaso@gmail.com>
2016-03-16 03:06:10 +0900

    ホイールの処理方法を変更

--- MouseHook/main.cpp
+++ MouseHook/main.cpp
@@ -3,10 +3,14 @@
 
 #include <string>
 
+#define FORWARD_WM_MOUSEHWHEEL(hwnd, xPos, yPos, zDelta, fwKeys, fn) \
+    (void)(fn)((hwnd), WM_MOUSEHWHEEL, MAKEWPARAM((fwKeys),(zDelta)), MAKELPARAM((xPos),(yPos)))
+
+
 HINSTANCE hinst;
 HHOOK hhook;
 
-LRESULT CALLBACK keyboard_proc(
+LRESULT CALLBACK mouse_proc(
 	_In_ int    nCode,
 	_In_ WPARAM wParam,
 	_In_ LPARAM lParam
@@ -28,7 +32,7 @@ BOOL WINAPI DllMain(
 __declspec(dllexport)
 bool start_hook()
 {
-	hhook = ::SetWindowsHookEx(WH_MOUSE_LL, ::keyboard_proc, ::hinst, 0);
+	hhook = ::SetWindowsHookEx(WH_MOUSE_LL, ::mouse_proc, ::hinst, 0);
 	return hhook != nullptr;
 }
 
@@ -39,110 +43,117 @@ void end_hook()
 		::UnhookWindowsHookEx(hhook);
 }
 
-MSLLHOOKSTRUCT s;
 HWND htarget;
 POINT presspos;
 bool pressing = false;
 bool moved;
 bool toward, right;
+bool sended = false;
 
-LRESULT CALLBACK keyboard_proc(
+LRESULT CALLBACK mouse_proc(
 	_In_ int    nCode,
 	_In_ WPARAM wParam,
 	_In_ LPARAM lParam
 )
 {
-	if (wParam == WM_MBUTTONDOWN) {
-		if (::GetCursorPos(&presspos) != FALSE) {
-			HWND hwnd = htarget;
-			POINT pt = presspos;
+	if (nCode == HC_ACTION) {
+		if (wParam == WM_MBUTTONDOWN) {
+			if (!sended) {
+				if (::GetCursorPos(&presspos) != FALSE)
+				{
+					HWND hwnd = htarget;
+					POINT pt = presspos;
 
-			hwnd = ::WindowFromPoint(pt);
+					hwnd = ::WindowFromPoint(pt);
 
-			do {
-				htarget = hwnd;
+					do {
+						htarget = hwnd;
 
+						::ScreenToClient(htarget, &pt);
+						hwnd = ::ChildWindowFromPointEx(htarget, pt, CWP_ALL);
+					} while (hwnd != nullptr && hwnd != htarget);
+
+					pressing = true;
+					moved = false;
+				}
+				
+				return 1;
+			}
+		}
+		else if (wParam == WM_MBUTTONUP) {
+			pressing = false;
+
+			if (sended) {
+				sended = false;
+				return ::CallNextHookEx(hhook, nCode, wParam, lParam);
+			}
+			else if (!moved) {
+				INPUT i[2] = {};
+				i[0].type = INPUT_MOUSE;
+				i[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN | MOUSEEVENTF_ABSOLUTE;
+				i[0].mi.dx = presspos.x;
+				i[0].mi.dy = presspos.y;
+
+				i[1].type = INPUT_MOUSE;
+				i[1].mi.dwFlags = MOUSEEVENTF_MIDDLEUP | MOUSEEVENTF_ABSOLUTE;
+				i[1].mi.dx = presspos.x;
+				i[1].mi.dy = presspos.y;
+
+				::SendInput(2, i, sizeof(INPUT));
+				sended = true;
+			}
+			
+			return 1;
+		}
+		else if (pressing && wParam == WM_MOUSEMOVE) {
+			moved = true;
+
+			auto lphook = reinterpret_cast<LPMSLLHOOKSTRUCT>(lParam);
+
+			auto dx = lphook->pt.x - presspos.x;
+			auto dy = lphook->pt.y - presspos.y;
+
+			INPUT i = {};
+
+			if (right == (dx > 0) && dx != 0) {
+				DWORD delta = WHEEL_DELTA * dx / 5;
+
+				if (dx < 0)
+					delta--;
+				else
+					delta++;
+
+				POINT pt = presspos;
 				::ScreenToClient(htarget, &pt);
-				hwnd = ::ChildWindowFromPointEx(htarget, pt, CWP_ALL);
-			} while (hwnd != nullptr && hwnd != htarget);
 
-			pressing = true;
-			moved = false;
+				//FORWARD_WM_MOUSEHWHEEL(htarget, pt.x, pt.y, delta, 0, ::PostMessage);
 
-			s = *(MSLLHOOKSTRUCT*)lParam;
+				//htarget = ::GetAncestor(htarget, GA_PARENT);
+				//HWND hparent = ::GetAncestor(htarget, GA_PARENT);
 
-			wchar_t a[100];
-			wsprintf(a, L"%x\n", htarget);
-			::OutputDebugString(a);
-		}
+				//FORWARD_WM_HSCROLL(hparent, htarget, (dx > 0 ? SB_LINERIGHT : SB_LINELEFT), 0, ::PostMessage);
+				//FORWARD_WM_HSCROLL(hparent, htarget, SB_ENDSCROLL, 0, ::PostMessage);
+			}
 
-		if (nCode >= 0)
+			if (toward == (dy < 0) && dy != 0) {
+				DWORD delta = -WHEEL_DELTA * dy / 5;
+
+				if (dy < 0)
+					delta++;
+				else
+					delta--;
+
+				POINT pt = presspos;
+				::ScreenToClient(htarget, &pt);
+
+				FORWARD_WM_MOUSEWHEEL(htarget, pt.x, pt.y, delta, 0, ::PostMessage);
+			}
+
+			right = dx > 0;
+			toward = dy < 0;
+			
 			return 1;
-	}
-	else if (wParam == WM_MBUTTONUP) {
-		pressing = false;
-
-		if (!moved) {
-			INPUT i[2] = {};
-			i[0].type = INPUT_MOUSE;
-			i[0].mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN | MOUSEEVENTF_ABSOLUTE;
-			i[0].mi.dx = presspos.x;
-			i[0].mi.dy = presspos.y;
-
-			i[1].type = INPUT_MOUSE;
-			i[1].mi.dwFlags = MOUSEEVENTF_MIDDLEUP | MOUSEEVENTF_ABSOLUTE;
-			i[1].mi.dx = presspos.x;
-			i[1].mi.dy = presspos.y;
-
-			::SendInput(2, i, sizeof(INPUT));
 		}
-
-		if (nCode >= 0)
-			return 1;
-	}
-	else if (pressing && wParam == WM_MOUSEMOVE) {
-		moved = true;
-
-		auto lphook = reinterpret_cast<LPMSLLHOOKSTRUCT>(lParam);
-
-		auto dx = lphook->pt.x - presspos.x;
-		auto dy = lphook->pt.y - presspos.y;
-
-		INPUT i = {};
-
-		if (right == (dx > 0) &&dx != 0) {
-			DWORD delta = WHEEL_DELTA * dx / 4;
-
-			if (dx < 0)
-				delta--;
-			else
-				delta++;
-
-			i.type = INPUT_MOUSE;
-			i.mi.dwFlags = MOUSEEVENTF_HWHEEL;
-			i.mi.mouseData = delta;
-			::SendInput(1, &i, sizeof(INPUT));
-		}
-
-		if(toward == (dy < 0) && dy != 0){
-			DWORD delta = - WHEEL_DELTA * dy / 4;
-
-			if (dy < 0)
-				delta++;
-			else
-				delta--;
-
-			i.type = INPUT_MOUSE;
-			i.mi.dwFlags = MOUSEEVENTF_WHEEL;
-			i.mi.mouseData = delta;
-			::SendInput(1, &i, sizeof(INPUT));
-		}
-
-		right = dx > 0;
-		toward = dy < 0;
-
-		if(nCode >= 0)
-			return 1;
 	}
 
 	return ::CallNextHookEx(hhook, nCode, wParam, lParam);
